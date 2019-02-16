cdf699b1e85eabc5a25957892332d6f5fd49b609
BrkSe <brkktby@gmail.com>
2019-02-16 16:53:59 +0300

    User Controller eklendi.

---
+++ src/main/java/com/burakkutbay/springkayitgirisuygulamasi/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.burakkutbay.springkayitgirisuygulamasi.controller;
+
+import com.burakkutbay.springkayitgirisuygulamasi.model.User;
+import com.burakkutbay.springkayitgirisuygulamasi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+/**
+ * Created by burakkutbay on 14.02.2019.
+ */
+
+@Controller
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registerPage(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String saveRegisterPage(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) {
+        model.addAttribute("user", user);
+
+        if (result.hasErrors()) {
+            return "register";
+        } else {
+            userService.saveUser(user);
+
+        }
+        return "index";
+    }
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/index")
+    public String index2() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/secure")
+    public String secure() {
+        return "secure";
+    }
+}
