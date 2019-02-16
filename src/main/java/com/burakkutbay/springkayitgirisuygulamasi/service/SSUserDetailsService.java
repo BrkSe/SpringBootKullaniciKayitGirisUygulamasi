5192b2040adecb7a7889c072e2e661948d4bf1e4
BrkSe <brkktby@gmail.com>
2019-02-16 16:56:35 +0300

    User Detail Service oluşturuldu.

---
+++ src/main/java/com/burakkutbay/springkayitgirisuygulamasi/service/SSUserDetailsService.java
@@ -0,0 +1,56 @@
+package com.burakkutbay.springkayitgirisuygulamasi.service;
+
+import com.burakkutbay.springkayitgirisuygulamasi.model.Role;
+import com.burakkutbay.springkayitgirisuygulamasi.model.User;
+import com.burakkutbay.springkayitgirisuygulamasi.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by burakkutbay on 14.02.2019.
+ */
+
+@Transactional
+@Service
+public class SSUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public SSUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            User user = userRepository.findByUsername(username);
+            if (user == null) {
+                return null;
+            }
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    getAuthorities(user));
+
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+    private Set<GrantedAuthority> getAuthorities(User user) {
+        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+        for (Role role : user.getRoles()) {
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getRole());
+            authorities.add(grantedAuthority);
+        }
+        return authorities;
+
+    }
+}
